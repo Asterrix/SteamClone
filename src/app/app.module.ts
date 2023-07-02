52335b456ffcc9a90ab25fac6ee4f4506c56195b
Asterrix <nezirevic.enis@outlook.com>
2023-07-02 03:07:48 +0200

    Created a header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {HeaderComponent} from './components/header/header.component';
+import {NgOptimizedImage} from "@angular/common";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    NgOptimizedImage,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,58 @@
+import {Component, HostListener, OnInit} from '@angular/core';
+import {animate, animateChild, keyframes, query, state, style, transition, trigger} from "@angular/animations";
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  animations: [
+    trigger('dumbParent', [
+      transition('* => void', [
+        query('@*', [animateChild()], {optional: true})
+      ])
+    ]),
+    trigger('slideRight', [
+      transition('void => *', [
+        style({transform: 'translateX(-100%)'}),
+        animate('0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940)')
+      ]),
+      transition('* => void', [
+        style({transform: 'translateX(0)'}),  // Start from 0
+        animate('0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940)', keyframes([
+          style({transform: 'translateX(0)', offset: 0}),
+          style({transform: 'translateX(-100%)', offset: 1})
+        ]))
+      ])
+    ]),
+  ]
+})
+export class HeaderComponent implements OnInit {
+  private _mobileView: boolean = false;
+  private _sidebarOpen: boolean = false;
+  private _maxMobileResolution: number = 1024;
+
+  get mobileView(): boolean {
+    return this._mobileView;
+  }
+
+  get sidebarOpen(): boolean {
+    return this._sidebarOpen;
+  }
+
+  ngOnInit(): void {
+    this.calculateScreenWidth();
+  }
+
+  public toggleSidebar(): void {
+    this._sidebarOpen = !this._sidebarOpen;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  private onResize(): void {
+    this.calculateScreenWidth();
+  }
+
+  private calculateScreenWidth(): void {
+    this._mobileView = window.innerWidth < this._maxMobileResolution;
+  }
+}
